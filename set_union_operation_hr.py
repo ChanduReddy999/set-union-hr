35736ebfeadf13089a690a23f68b7495ffac683e
ChanduReddy999 <113517229+ChanduReddy999@users.noreply.github.com>
2022-10-21 23:30:35 +0530

    Add files via upload

---
+++ set_union_operation_hr.py
@@ -0,0 +1,13 @@
+"""Performing set union operation in Python(HackerRank)"""
+
+# n = int(input())
+# l = list(input().split())
+# m = int(input())
+# k = list(input().split())
+
+# s1 = set(l)
+# s2 = set(k)
+
+# print(len(s1.union(s2)))
+
+
